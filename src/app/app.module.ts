27fd0b8cc6a7566088a392269c1e8d8aa51e4c08
macbrack <lordcratustmc@gmail.com>
2021-09-22 20:21:53 -0500

    Basic template for the app done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyViewerComponent } from './properties/property-viewer/property-viewer.component';
+import { HomeComponent } from './home/home.component';
+import { PropertyBrowserComponent } from './properties/property-browser/property-browser.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyViewerComponent,
+    HomeComponent,
+    PropertyBrowserComponent,
+    PropertiesComponent,
+    HeaderComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
