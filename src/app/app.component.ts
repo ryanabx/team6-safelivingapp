123ed51a64f9bfcf0c72e4016ad8e101d426dc15
LoganCornwell <90277465+LoganCornwell@users.noreply.github.com>
2021-10-11 17:52:21 -0500

    e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, Injectable, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AppService } from './app.service';
+import { AddrInputService } from './addr-input.service';
+
+/*
+@Injectable({
+  providedIn: 'root'
+})*/
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'tusafeliving';
+  public inputAddr: any;
+
+constructor(
+  private appService: AppService,
+  private addrInputService: AddrInputService, 
+  private router: Router,){}
+
+  sendInput(value: string) {
+    this.inputAddr = value;
+    console.log(this.inputAddr);
+    this.addrInputService.setAddr(this.inputAddr);
+    this.router.navigateByUrl('/coord-list');
+   }
+
+  ngOnInit(): void {
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './app.service';
+
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoordListComponent } from './coord-list/coord-list.component';
+import { AddrInputService } from './addr-input.service';
 
+// array for easy management of routes
+// runGuardsAndResolvers used in reloading components
 const routes: Routes = [
-  {path: 'coord-list', component: CoordListComponent}
+  {path: 'coord-list', component: CoordListComponent,
+    
+   runGuardsAndResolvers: 'always'}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
+    CoordListComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    //onSameUrlNavigation used for reloading routes when already on them
+    RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'}), 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AppService, AddrInputService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
