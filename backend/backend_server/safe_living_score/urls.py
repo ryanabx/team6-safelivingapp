c729b9958e26084c200f760cb2dad8f9b9eda5f5
ryanbrue <56272643+ryanbrue@users.noreply.github.com>
2021-11-29 18:44:15 -0600

    Backend functions to get different crime scores by type

--- backend/backend_server/safe_living_score/urls.py
+++ backend/backend_server/safe_living_score/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/<str:lat>/<str:lon>/<str:radius>/', views.getScore),
+    path('api/<str:lat>/<str:lon>/<str:radius>/', views.getScoreAll),
+    path('api/<str:lat>/<str:lon>/<str:radius>/<str:crime_type>', views.getScore),
     path('api/<str:ORI>/', views.getScorebyORI),
 ]
