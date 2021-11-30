07c5afc515f14c18de615ddfe6a05dea7ace0f08
Sterfry42 <sjf2864@utulsa.edu>
2021-11-29 20:18:26 -0600

    Changed App Name

--- backend/backend_server/city_boundaries/apps.py
+++ backend/backend_server/city_boundaries/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CityboundariesConfig(AppConfig):
+class CityBoundariesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cityBoundaries'
+    name = 'city_boundaries'
