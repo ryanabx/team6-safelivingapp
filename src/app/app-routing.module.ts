7cfcee53c5cdeb857cfb128cac69626378bf3d93
ryanbrue <56272643+ryanbrue@users.noreply.github.com>
2022-03-08 18:19:21 -0600

    Removed unnecessary components from frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { PropertiesComponent } from "./properties/properties.component";
-import { PropertyBrowserComponent } from "./properties/property-browser/property-browser.component";
-import { PropertyViewerComponent } from "./properties/property-viewer/property-viewer.component";
 import { HomeComponent } from './home/home.component';
 import { MapComponent } from './map/map.component';
-import { CrimeapiComponent } from './crimeapi/crimeapi.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
@@ -16,11 +12,6 @@ const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'map', component: MapComponent },
-    { path: 'property', component: PropertiesComponent, children: [
-        { path: '', component: PropertyBrowserComponent },
-        { path: ':id', component: PropertyViewerComponent }
-    ]},
-    { path: 'crimeapi', component: CrimeapiComponent},
     { path: 'create', component: CreateComponent },
     { path: 'login', component: LoginComponent },
     { path: 'about', component: AboutComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
-import { PropertyViewerComponent } from './properties/property-viewer/property-viewer.component';
 import { HomeComponent } from './home/home.component';
-import { PropertyBrowserComponent } from './properties/property-browser/property-browser.component';
-import { PropertiesComponent } from './properties/properties.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MapComponent } from './map/map.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CoordListComponent } from './coord-list/coord-list.component';
 import { AddrInputService } from './addr-input.service';
 import { AppService } from './app.service';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './user.service';
-import { CrimeapiComponent } from './crimeapi/crimeapi.component';
 import { UserBookmarksComponent } from './user-bookmarks/user-bookmarks.component';
 import { CreateComponent } from './create/create.component';
 import { LoginComponent } from './login/login.component';
@@ -27,14 +22,9 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CoordListComponent,
-    PropertiesComponent,
     HeaderComponent,
     MapComponent,
     HomeComponent,
-    PropertyViewerComponent,
-    PropertyBrowserComponent,
-    CrimeapiComponent,
     UserBookmarksComponent,
     CreateComponent,
     LoginComponent,
