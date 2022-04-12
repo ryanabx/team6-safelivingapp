671a5c88e0e9c778474bae3900d0919d10684922
Sterfry42 <56270865+Sterfry42@users.noreply.github.com>
2022-04-12 10:37:21 -0500

    Task done.

--- backend/backend_server/recommendations/urls.py
+++ backend/backend_server/recommendations/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/radius/<str:initialAddress>/<str:radiusValue>/<str:minPopulation>', views.recommendCity),
-    path('api/radius/<str:initialAddress>/<str:radiusValue>/<str:minPopulation>/<str:maxPopulation>', views.recommendCity),
-    path('api/radius/<str:initialAddress>/<str:radiusValue>', views.recommendCity),
+    path('api/radius/<str:scoreCategory>/<str:initialAddress>/<str:radiusValue>/<str:minPopulation>', views.recommendCity),
+    path('api/radius/<str:scoreCategory>/<str:initialAddress>/<str:radiusValue>/<str:minPopulation>/<str:maxPopulation>', views.recommendCity),
+    path('api/radius/<str:scoreCategory>/<str:initialAddress>/<str:radiusValue>', views.recommendCity),
     
-    path('api/state/<str:stateID>', views.recommendCity),
-    path("api/state/<str:stateID>/<str:minPopulation>", views.recommendCity),
-    path("api/state/<str:stateID>/<str:minPopulation>/<str:maxPopulation>", views.recommendCity)
+    path('api/state/<str:scoreCategory>/<str:stateID>', views.recommendCity),
+    path("api/state/<str:scoreCategory>/<str:stateID>/<str:minPopulation>", views.recommendCity),
+    path("api/state/<str:scoreCategory>/<str:stateID>/<str:minPopulation>/<str:maxPopulation>", views.recommendCity)
 ]
+
+#http://localhost:8000/recommendations/api/radius/safe-living/tulsa/200
