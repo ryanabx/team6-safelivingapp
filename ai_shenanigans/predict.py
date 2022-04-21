455f22bb944b8ea2fbd1096f0c7d304ac1e657a8
Jacob-AB <bruejacob23@gmail.com>
2022-04-21 14:35:40 -0500

    Model designed
    Has not been ran for testing yet. Might need beefy GPU

--- ai_shenanigans/predict.py
+++ ai_shenanigans/predict.py
@@ -1,27 +1,45 @@
-import numpy
 import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
+from keras.losses import MeanSquaredError
+from keras.callbacks import ModelCheckpoint
+from keras.models import *
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dropout
+from keras.layers import Dense
 
-def get_crime_data(year_count = 5):
-    with open('') as crime_file:
-        crime_data = json.load(crime_file)    
-        data_vects = []
-        for ori, ori_data in ori_data.items():
-            annual_vects = []
-            years = []
-            for year, year_data in crime_data.items():
-                v = numpy.array(year_data.values())
-                annual_vects[year] = v
-                years.append(year)
-            for year in years:
-                if not set(range(year, year + year_count)).issubset(set(years)):
-                    continue
-                v = []
-                for year in range(year, year + year_count):
-                    v.append(annual_vects[year])
-                data_vects.append(v)
-            
+def get_network():
+    sample_rows = 5
+    sample_cols = 12
+    inputs = Input((sample_rows * sample_cols))
+    hide1 = Dense(32, activation='relu', kernel_initializer='he_normal')(inputs)
+    hide2 = Dense(16, activation='relu', kernel_initializer='he_normal')(hide1)
+    hide3 = Dense(8, activation='relu', kernel_initializer='he_normal')(hide2)
+    out = Dense(1, activation='linear', kernel_initializer='he_normal')(hide3)
+    model = Model(inputs=inputs, outputs=out)
+    model.compile(optimizer=Adam(lr=1e-4), loss='mse')
+    return model
 
+def main():
+    model = get_network()
+    data_dir = 'backend/backend_server.datasets/'
+    x = np.load(data_dir + 'x.npy')
+    y = np.load(data_dir + 'y.npy')
+    g = np.load(data_dir + 'g.npy')
+    g_lable = np.load(data_dir + 'g_label.npy')
 
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
+    model_checkpoint = ModelCheckpoint('model location', monitor='val_loss', verbose=1, save_best_only=True)
+    hist = model.fit(x_train, y_train, batch_size=4, epochs=20, verbose=1, validation_split=0.1,
+                     shuffle=True, callbacks=[model_checkpoint])
+    y_pred = model.predict(x_test, batch_size=1, verbose=1)
+    mse = MeanSquaredError()(y_test, y_pred).numpy()
 
+    print('hist')
+    print(hist)
+    print('mse')
+    print(mse)
 
+if __name__ == "__main__":
+    main()
